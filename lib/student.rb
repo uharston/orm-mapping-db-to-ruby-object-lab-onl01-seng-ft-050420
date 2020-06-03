3210be46a0aec1604d46fa054a24041f053028d4
Uriah Harston <uharston@gmail.com>
2020-06-03 16:24:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ class Student
   #   DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
   # end 
   
-  def 
+  
 
   def self.first_student_in_grade_10
     student = self.first_X_students_in_grade_10(row)
