aae6c26f6530283bbded72fc467f20747f9b43d0
Uriah Harston <uharston@gmail.com>
2020-06-03 16:25:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,15 +70,15 @@ class Student
     DB[:conn].execute(sql)
   end 
   
-  # def self.students_below_12th_grade 
-  #   sql = <<-SQL
-  #     SELECT * 
-  #     FROM students 
-  #     WHERE grade < 12 
-  #   SQL
+  def self.students_below_12th_grade 
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade < 12 
+    SQL
     
-  #   DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
-  # end 
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
+  end 
   
   
 
