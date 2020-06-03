71a964fa471d2fdbb64864b1ee518ac795c1f077
Uriah Harston <uharston@gmail.com>
2020-06-03 16:18:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,15 +88,15 @@ def self.students_below_12th_grade
   end
 
   def self.first_student_in_grade_10
-    s = self.first_X_students_in_grade_10(1)
-    s.map do |i|
-      self.new_from_db(i)
-    end[0]
+    student = self.first_X_students_in_grade_10(row)
+    student.map { |row| self.new_from_db(row)}.first 
   end
 
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
-       SELECT * FROM students WHERE grade = ?
+       SELECT * 
+       FROM students 
+       WHERE grade = ?
     SQL
 
     DB[:conn].execute(sql, grade)
